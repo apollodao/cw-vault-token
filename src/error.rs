52f1bb0f0a3a85699b2501696fb9c9bbcb23448c
Sturdy <sturdy@apollo.farm>
2022-09-20 19:18:03 +0200

    fix: derive from error

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,5 @@
 use cosmwasm_std::{Response, StdError};
-use cw20_base::ContractError;
+use cw20_base::ContractError as Cw20ContractError;
 use thiserror::Error;
 
 /// ## Description
@@ -13,14 +13,9 @@ pub enum CwTokenError {
     /// Invalid Reply ID Error
     #[error("invalid reply id")]
     InvalidReplyId {},
-    // Add any other custom errors you like here.
-    // Look at https://docs.rs/thiserror/1.0.31/thiserror/ for details.
-}
 
-impl From<ContractError> for CwTokenError {
-    fn from(x: ContractError) -> Self {
-        CwTokenError::Std(StdError::generic_err(x.to_string()))
-    }
+    #[error("{0}")]
+    Cw20ContractError(#[from] Cw20ContractError),
 }
 
 pub type CwTokenResult<T> = Result<T, CwTokenError>;
