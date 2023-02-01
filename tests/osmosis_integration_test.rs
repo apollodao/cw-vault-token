8e1222ec8567f439380c0a0e29aa3003be05b0a5
Pacman <pacman@apollo.farm>
2023-02-01 13:06:47 +0100

    test(OsmosisDenom): refactor testing robot

--- tests/osmosis_integration_test.rs
+++ tests/osmosis_integration_test.rs
@@ -5,12 +5,12 @@ use cosmwasm_std::{
 };
 
 use cw_vault_token::osmosis::OsmosisDenom;
-use cw_vault_token::{Burn, Instantiate, Mint, VaultToken};
+use cw_vault_token::VaultToken;
 
 use osmosis_testing::osmosis_std::types::osmosis::tokenfactory::v1beta1::{
     MsgBurnResponse, MsgCreateDenomResponse, MsgMintResponse,
 };
-use osmosis_testing::{Account, ExecuteResponse, Module, OsmosisTestApp, Runner, SigningAccount};
+use osmosis_testing::{Account, OsmosisTestApp, Runner, SigningAccount};
 
 use test_case::test_case;
 
@@ -40,15 +40,13 @@ fn mock_env_with_address(deps: Deps, address: &str) -> Env {
 }
 
 #[derive(Clone, Debug)]
-struct TokenRobot<'a, R: Runner<'a> + Querier, S: ::prost::Message, T: VaultToken + Clone> {
+struct TokenRobot<'a, R: Runner<'a> + Querier, T: VaultToken + Clone> {
     app: &'a R,
     denom: &'a T,
     last_events: Vec<Event>,
 }
 
-impl<'a, R: Runner<'a> + Querier, S: ::prost::Message, T: VaultToken + Clone>
-    TokenRobot<'a, R, S, T>
-{
+impl<'a, R: Runner<'a> + Querier, T: VaultToken + Clone> TokenRobot<'a, R, T> {
     pub fn new(app: &'a R, denom: &'a T) -> Self {
         Self {
             app,
@@ -57,24 +55,13 @@ impl<'a, R: Runner<'a> + Querier, S: ::prost::Message, T: VaultToken + Clone>
         }
     }
 
-    fn instantiate<S: ::prost::Message + Default>(
-        self,
-        signer: &SigningAccount,
-    ) -> (Self, ExecuteResponse<S>) {
-        let sub_msgs = self
+    fn instantiate<S: ::prost::Message + Default>(self, signer: &SigningAccount) -> Self {
+        let response = self
             .denom
             .instantiate(mock_dependencies().as_mut(), None)
-            .unwrap()
-            .messages;
-
-        let cosmos_msgs: Vec<CosmosMsg> = sub_msgs.into_iter().map(|x| x.msg).collect();
-
-        let res = self
-            .app
-            .execute_cosmos_msgs::<S>(&cosmos_msgs, signer)
             .unwrap();
 
-        (self, res)
+        self.execute_response::<S>(signer, response)
     }
 
     fn mint<S: ::prost::Message + Default>(
@@ -87,36 +74,21 @@ impl<'a, R: Runner<'a> + Querier, S: ::prost::Message, T: VaultToken + Clone>
         let env = mock_env_with_address(deps.as_ref(), &signer.address());
 
         let recipient = deps.api.addr_validate(recipient).unwrap();
-        self.execute_response(
-            signer,
-            self.denom
-                .mint(deps.as_mut(), &env, &recipient, amount)
-                .unwrap(),
-        )
+        let response = self
+            .denom
+            .clone()
+            .mint(deps.as_mut(), &env, &recipient, amount)
+            .unwrap();
+        self.execute_response::<S>(signer, response)
     }
 
-    fn burn<S: ::prost::Message + Default>(
-        self,
-        signer: &SigningAccount,
-        amount: Uint128,
-    ) -> (Self, ExecuteResponse<S>) {
+    fn burn<S: ::prost::Message + Default>(self, signer: &SigningAccount, amount: Uint128) -> Self {
         let mut deps = mock_dependencies();
         let env = mock_env_with_address(deps.as_ref(), &signer.address());
 
-        let sub_messages = self
-            .denom
-            .burn(deps.as_mut(), &env, amount)
-            .unwrap()
-            .messages;
+        let response = self.denom.burn(deps.as_mut(), &env, amount).unwrap();
 
-        let cosmos_msgs: Vec<CosmosMsg> = sub_messages.into_iter().map(|x| x.msg).collect();
-
-        let res = self
-            .app
-            .execute_cosmos_msgs::<S>(&cosmos_msgs, signer)
-            .unwrap();
-
-        (self, res)
+        self.execute_response::<S>(signer, response)
     }
 
     fn query_balance(&self, address: &str) -> Uint128 {
@@ -131,7 +103,11 @@ impl<'a, R: Runner<'a> + Querier, S: ::prost::Message, T: VaultToken + Clone>
         self.denom.query_total_supply(deps.as_ref()).unwrap()
     }
 
-    fn execute_response(mut self, signer: &SigningAccount, res: Response) -> Self {
+    fn execute_response<S: ::prost::Message + Default>(
+        mut self,
+        signer: &SigningAccount,
+        res: Response,
+    ) -> Self {
         let cosmos_msgs: Vec<CosmosMsg> = res.messages.into_iter().map(|x| x.msg).collect();
 
         let execute_res = self
@@ -144,25 +120,12 @@ impl<'a, R: Runner<'a> + Querier, S: ::prost::Message, T: VaultToken + Clone>
         self
     }
 
-    fn assert_event(&self, expected_type: &str, expected_attributes: Vec<Attribute>) -> Self {
-        let mut responses = self.last_events.to_vec();
-        responses.reverse();
-
-        for res in responses.iter() {
-            if let Some(event) = res.events.iter().find(|e| e.ty == expected_type) {
-                if event.attributes.len() == expected_attributes.len() {
-                    for (attr, expected_attr) in
-                        zip(event.attributes.iter(), expected.attributes.iter())
-                    {
-                        if attr.key != expected_attr.key || attr.value != expected_attr.value {
-                            continue;
-                        }
-                    }
-                }
-            }
+    fn assert_event(self, expected_type: &str, expected_attributes: Vec<Attribute>) -> Self {
+        let expected_event = &Event::new(expected_type).add_attributes(expected_attributes);
+        match self.last_events.contains(expected_event) {
+            true => self,
+            false => panic!("Event not found. Expected {:?}", expected_event),
         }
-
-        assert_eq!(events, expected);
     }
 }
 
@@ -212,7 +175,6 @@ fn mint(signer_idx: usize, amount: Uint128) {
 
     TokenRobot::new(&app, &denom)
         .instantiate::<MsgCreateDenomResponse>(creator)
-        .0
         .mint::<MsgMintResponse>(signer, &recipient.address(), amount)
         .assert_event(
             "tf_mint",
@@ -242,26 +204,17 @@ fn burn(signer_idx: usize, recipient_idx: usize, amount: Uint128) {
     let recipient = &accs[recipient_idx];
     let denom = OsmosisDenom::new(creator.address(), SUBDENOM.to_string());
 
-    let (_, burn_res) = TokenRobot::new(&app, &denom)
+    TokenRobot::new(&app, &denom)
         .instantiate::<MsgCreateDenomResponse>(creator)
-        .0
         .mint::<MsgMintResponse>(creator, &recipient.address(), Uint128::from(1000000u128))
-        .burn::<MsgBurnResponse>(signer, amount);
-
-    let burn_event = burn_res
-        .events
-        .into_iter()
-        .filter(|r| r.ty == "tf_burn")
-        .collect::<Vec<Event>>();
-
-    let expected_event = Event::new("tf_burn".to_string()).add_attributes(vec![
-        attr("burn_from_address", creator.address()),
-        attr("amount", format!("{}{}", amount, denom)),
-    ]);
-
-    // Check that the burn token event is emitted
-    assert_eq!(burn_event.len(), 1);
-    assert_eq!(burn_event[0], expected_event);
+        .burn::<MsgBurnResponse>(signer, amount)
+        .assert_event(
+            "tf_burn",
+            vec![
+                attr("burn_from_address", creator.address()),
+                attr("amount", format!("{}{}", amount, denom)),
+            ],
+        );
 }
 
 #[test_case(0 ; "total supply")]
@@ -275,7 +228,6 @@ fn query(query: usize) {
 
     let robot = TokenRobot::new(&app, &denom)
         .instantiate::<MsgCreateDenomResponse>(creator)
-        .0
         .mint::<MsgMintResponse>(creator, &recipient.address(), amount);
 
     let query_result = match query {
