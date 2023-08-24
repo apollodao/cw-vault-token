196fbb02dfb2b40af0b2a170567ca6511ddf02a2
Sturdy <91910406+apollo-sturdy@users.noreply.github.com>
2023-08-24 13:37:47 +0200

    test: Check balance in mint and burn tests

--- tests/osmosis_integration_test.rs
+++ tests/osmosis_integration_test.rs
@@ -1,6 +1,7 @@
 use cosmwasm_std::testing::{mock_dependencies, mock_env};
 use cosmwasm_std::{attr, Api, Attribute, Coin, CosmosMsg, Deps, Env, Event, Response, Uint128};
 
+use cw_it::robot::TestRobot;
 use cw_vault_token::osmosis::OsmosisDenom;
 use cw_vault_token::{Burn, Instantiate, Mint, VaultToken};
 
@@ -44,6 +45,15 @@ struct TokenRobot<'a, R: Runner<'a>, T: VaultToken + Clone> {
     last_events: Vec<Event>,
 }
 
+impl<'a, R> TestRobot<'a, R> for TokenRobot<'a, R, OsmosisDenom>
+where
+    R: Runner<'a>,
+{
+    fn runner(&self) -> &'a R {
+        self.app
+    }
+}
+
 impl<'a, R: Runner<'a>> TokenRobot<'a, R, OsmosisDenom> {
     pub fn new(app: &'a R, denom: &'a OsmosisDenom) -> Self {
         Self {
@@ -53,7 +63,7 @@ impl<'a, R: Runner<'a>> TokenRobot<'a, R, OsmosisDenom> {
         }
     }
 
-    fn instantiate<S: ::prost::Message + Default>(self, signer: &SigningAccount) -> Self {
+    fn instantiate<S: ::prost::Message + Default>(&mut self, signer: &SigningAccount) -> &mut Self {
         let response = self
             .denom
             .instantiate(mock_dependencies().as_mut(), None)
@@ -63,11 +73,11 @@ impl<'a, R: Runner<'a>> TokenRobot<'a, R, OsmosisDenom> {
     }
 
     fn mint<S: ::prost::Message + Default>(
-        self,
+        &mut self,
         signer: &SigningAccount,
         recipient: &str,
         amount: Uint128,
-    ) -> Self {
+    ) -> &mut Self {
         let mut deps = mock_dependencies();
         let env = mock_env_with_address(deps.as_ref(), &signer.address());
 
@@ -80,7 +90,11 @@ impl<'a, R: Runner<'a>> TokenRobot<'a, R, OsmosisDenom> {
         self.execute_response::<S>(signer, response)
     }
 
-    fn burn<S: ::prost::Message + Default>(self, signer: &SigningAccount, amount: Uint128) -> Self {
+    fn burn<S: ::prost::Message + Default>(
+        &mut self,
+        signer: &SigningAccount,
+        amount: Uint128,
+    ) -> &mut Self {
         let mut deps = mock_dependencies();
         let env = mock_env_with_address(deps.as_ref(), &signer.address());
 
@@ -90,10 +104,10 @@ impl<'a, R: Runner<'a>> TokenRobot<'a, R, OsmosisDenom> {
     }
 
     fn execute_response<S: ::prost::Message + Default>(
-        mut self,
+        &mut self,
         signer: &SigningAccount,
         res: Response,
-    ) -> Self {
+    ) -> &mut Self {
         let cosmos_msgs: Vec<CosmosMsg> = res.messages.into_iter().map(|x| x.msg).collect();
 
         let execute_res = self
@@ -106,7 +120,7 @@ impl<'a, R: Runner<'a>> TokenRobot<'a, R, OsmosisDenom> {
         self
     }
 
-    fn assert_event(self, expected_type: &str, expected_attributes: Vec<Attribute>) -> Self {
+    fn assert_event(&self, expected_type: &str, expected_attributes: Vec<Attribute>) -> &Self {
         let expected_event = &Event::new(expected_type).add_attributes(expected_attributes);
         match self.last_events.contains(expected_event) {
             true => self,
@@ -136,24 +150,21 @@ fn mint(signer_idx: usize, amount: Uint128) {
     let recipient = &accs[1];
     let denom = OsmosisDenom::new(creator.address(), SUBDENOM.to_string());
 
-    TokenRobot::new(&app, &denom)
+    let mut robot = TokenRobot::new(&app, &denom);
+
+    robot
         .instantiate::<MsgCreateDenomResponse>(creator)
         .mint::<MsgMintResponse>(signer, &recipient.address(), amount)
-        .assert_event(
-            "tf_mint",
-            vec![
-                attr("mint_to_address", creator.address()),
-                attr("amount", format!("{}{}", amount, denom)),
-            ],
-        )
-        .assert_event(
-            "transfer",
-            vec![
-                attr("recipient", recipient.address()),
-                attr("sender", creator.address()),
-                attr("amount", format!("{}{}", amount, denom)),
-            ],
-        );
+        // TODO: Disabled because of bug in Osmosis TokenFactory:
+        // https://github.com/osmosis-labs/osmosis/issues/6147
+        // .assert_event(
+        //     "tf_mint",
+        //     vec![
+        //         attr("mint_to_address", recipient.address()),
+        //         attr("amount", format!("{}{}", amount, denom)),
+        //     ],
+        // );
+        .assert_native_token_balance_eq(recipient.address(), denom.to_string(), amount);
 }
 
 #[test_case(0, 0, Uint128::from(1000000u128) ; "executed by owner")]
@@ -167,9 +178,14 @@ fn burn(signer_idx: usize, recipient_idx: usize, amount: Uint128) {
     let recipient = &accs[recipient_idx];
     let denom = OsmosisDenom::new(creator.address(), SUBDENOM.to_string());
 
-    TokenRobot::new(&app, &denom)
+    let mut robot = TokenRobot::new(&app, &denom);
+
+    let recipient_balance_before = robot
         .instantiate::<MsgCreateDenomResponse>(creator)
         .mint::<MsgMintResponse>(creator, &recipient.address(), Uint128::from(1000000u128))
+        .query_native_token_balance(recipient.address(), denom.to_string());
+
+    robot
         .burn::<MsgBurnResponse>(signer, amount)
         .assert_event(
             "tf_burn",
@@ -177,5 +193,10 @@ fn burn(signer_idx: usize, recipient_idx: usize, amount: Uint128) {
                 attr("burn_from_address", creator.address()),
                 attr("amount", format!("{}{}", amount, denom)),
             ],
+        )
+        .assert_native_token_balance_eq(
+            recipient.address(),
+            denom.to_string(),
+            recipient_balance_before.checked_sub(amount).unwrap(),
         );
 }
